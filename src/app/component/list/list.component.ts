fdf8b3c9c873d216d634c9783c9cf943fc985767
Claudia Ton <claudia.ton@ibm.com>
2023-01-30 09:11:54 -0400

    Add unsubscribe

--- src/app/component/list/list.component.ts
+++ src/app/component/list/list.component.ts
@@ -1,5 +1,5 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { DataService } from 'src/app/service/data/data.service';
 import { getItems } from '../../store/actions';
 import { createSelector, select, Store } from '@ngrx/store';
@@ -13,16 +13,23 @@ import { PageEvent } from '@angular/material/paginator';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
-
-  onPaginate(pageEvent: PageEvent) {
-    this.recordsToShow = this.coffees.slice((pageEvent.pageIndex+1)*10-10,(pageEvent.pageIndex+1)*10)
-}
+export class ListComponent implements OnInit, OnDestroy {
 
   public getScreenWidth: any;
   public columns: number = 2;
   itemList$: Observable<Array<ICoffee>>;
-recordsToShow: Array<any> = [];
+  recordsToShow: Array<any> = [];
+
+  coffees: any;
+  subscription: Subscription = new Subscription();
+
+  constructor(private list: DataService, private store: Store<LibState>) {
+    this.itemList$ = this.store.pipe(select(getList));
+  }
+
+  onPaginate(pageEvent: PageEvent) {
+    this.recordsToShow = this.coffees.slice((pageEvent.pageIndex + 1) * 10 - 10, (pageEvent.pageIndex + 1) * 10)
+  }
 
   checkWindowWidth() {
     this.getScreenWidth = window.innerWidth;
@@ -33,25 +40,24 @@ recordsToShow: Array<any> = [];
       this.columns = 2;
     }
   }
-  coffees: any;
-
-  constructor(private list: DataService, private store: Store<LibState>) {
-    this.itemList$ = this.store.pipe(select(getList));
-  }
 
   ngOnInit() {
     this.checkWindowWidth();
 
     this.itemList$ = this.store.pipe(select(getList));
 
-this.store.dispatch(getItems())
+    this.store.dispatch(getItems())
 
-    this.itemList$.subscribe(
+    this.subscription = this.itemList$.subscribe(
       (response) => {
         this.coffees = response;
-        this.recordsToShow = this.coffees.slice(0,10)
+        this.recordsToShow = this.coffees.slice(0, 10)
       }
     )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   @HostListener('window:resize', ['$event'])
